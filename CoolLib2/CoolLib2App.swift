efb3115c220bb8013c7db5cb937a85e76f91f8de
susui <111405692+susui888@users.noreply.github.com>
2026-03-18 19:15:09 +0800

    Initial Commit

---
+++ CoolLib2/CoolLib2App.swift
@@ -0,0 +1,17 @@
+//
+//  CoolLib2App.swift
+//  CoolLib2
+//
+//  Created by susui on 2026/3/18.
+//
+
+import SwiftUI
+
+@main
+struct CoolLib2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
